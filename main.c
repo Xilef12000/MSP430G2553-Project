d935afb5ee0a03e595e8661a1212d0a3a8859bef
Manuel König <79515423+Xilef12000@users.noreply.github.com>
2025-01-23 08:53:58 +0100

    added attribution

--- main.c
+++ main.c
@@ -1,4 +1,11 @@
-// Motor
+//******************************
+// MPS Project - EIB3 - WiSe 23/24
+//
+// Niklas Bachman / 284301
+// Manuel König / 310284
+//
+// Motor Controller
+//******************************
 #include <msp430.h> 
 #include <stdint.h>
 
